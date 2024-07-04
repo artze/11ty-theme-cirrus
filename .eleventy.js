4e707c576fb3658496514b1c6cc82aedb5e7c7fa
artze <13703523+artze@users.noreply.github.com>
2024-07-04 20:47:49 +0800

    Minify css

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,24 @@
+const { mkdir, readFile, readdir, writeFile } = require("node:fs/promises");
+const CleanCss = require("clean-css");
+
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({
-    "./public/": "/",
+  /**
+   * Minify CSS
+   */
+  eleventyConfig.on("eleventy.after", async ({ dir }) => {
+    const cssFiles = await readdir(`${__dirname}/public/css`);
+    await mkdir(`${__dirname}/${dir.output}/css`, { recursive: true });
+    for (let i = 0; i < cssFiles.length; i++) {
+      const css = await readFile(`${__dirname}/public/css/${cssFiles[i]}`, {
+        encoding: "utf-8",
+      });
+      const cleanCssOutput = new CleanCss().minify(css);
+      await writeFile(
+        `${__dirname}/${dir.output}/css/${cssFiles[i]}`,
+        cleanCssOutput.styles
+      );
+    }
   });
 
   return {
