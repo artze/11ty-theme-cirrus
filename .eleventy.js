98fe11684677952547780b641cbc60cc1b8b2315
artze <13703523+artze@users.noreply.github.com>
2024-07-07 14:32:43 +0800

    Copy css without minifying if `runMode !== "build"`

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,20 @@
-const { mkdir, readFile, readdir, writeFile } = require("node:fs/promises");
+const { cp, mkdir, readFile, readdir, writeFile } = require("node:fs/promises");
 const CleanCss = require("clean-css");
 
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 module.exports = function (eleventyConfig) {
   /**
-   * Minify CSS
+   * Handle CSS assets
    */
-  eleventyConfig.on("eleventy.after", async ({ dir }) => {
-    const cssFiles = await readdir(`${__dirname}/public/css`);
+  eleventyConfig.on("eleventy.after", async ({ dir, runMode }) => {
+    if (runMode !== "build") {
+      await cp(`${__dirname}/public/css`, `${__dirname}/${dir.output}/css`, {
+        recursive: true,
+      });
+      return;
+    }
     await mkdir(`${__dirname}/${dir.output}/css`, { recursive: true });
+    const cssFiles = await readdir(`${__dirname}/public/css`);
     for (let i = 0; i < cssFiles.length; i++) {
       const css = await readFile(`${__dirname}/public/css/${cssFiles[i]}`, {
         encoding: "utf-8",
